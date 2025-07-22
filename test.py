cdb635a9a5dba28e6341006831d47e2cfed21dca
Seungho Jeong <snghojeong@gmail.com>
2025-07-22 22:46:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -88,7 +88,8 @@ def sum_primes_threaded(data_reader: DataReader, num_workers: int = 4) -> int:
 def main():
     """Main function to process data and print the results."""
     args = parse_args()
-    file_data_reader = FileDataReader(args.path) # no need for file://, its assumed its a local file
+    """ # no need for file://, its assumed its a local file """
+    file_data_reader = FileDataReader(args.path) 
     result = sum_primes_threaded(file_data_reader)
     print(f"Final Result: {result}")
 
