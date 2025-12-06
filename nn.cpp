9ea8f00f4e4b7f0d7e0782172da40506e4f4933c
Seungho Jeong <snghojeong@gmail.com>
2025-12-06 11:49:37 +0900

    Update nn.cpp

--- nn.cpp
+++ nn.cpp
@@ -130,7 +130,7 @@ static void simulate_input(ReactiveBus& bus){
 }
 
 int main(){
-    ReactiveBus bus(64); Image cur; std::atomic<bool> exit{false};
+    ReactiveBus bus(128); Image cur; std::atomic<bool> exit{false};
     auto commands = bus.observable()
         ->map<std::string>([](auto& e){return e.text;})
         ->throttle(std::chrono::milliseconds(20))
